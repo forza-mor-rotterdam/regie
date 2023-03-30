2f9c2c9f0d501f9b1d706d62c1ad909413004bd3
Remco Hoff <remco@code010.nl>
2023-03-30 16:20:38 +0200

    Showing all data and images

--- app/frontend/assets/controllers/detail_controller.js
+++ app/frontend/assets/controllers/detail_controller.js
@@ -2,8 +2,12 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
 
-    connect() {
+    static targets = ['area', 'district', 'selectedImage', 'thumbList', 'imageSliderContainer']
 
+    initialize() {
+        if(this.hasThumbListTarget) {
+            this.thumbListTarget.getElementsByTagName('li')[0].classList.add('selected')
+        }
     }
 
     openModal(e) {
@@ -22,4 +26,26 @@ export default class extends Controller {
         modalBackdrop.classList.remove('show');
         document.body.classList.remove('show-modal');
     }
+
+    onScrollSlider(e) {
+        this.highlightThumb(Math.floor(this.imageSliderContainerTarget.scrollLeft / this.imageSliderContainerTarget.offsetWidth))
+    }
+
+    selectImage(e) {
+        this.imageSliderContainerTarget.scrollTo({left: (Number(e.params.imageIndex) - 1) * this.imageSliderContainerTarget.offsetWidth, top: 0})
+        this.deselectThumbs(e.target.closest('ul'));
+        e.target.closest('li').classList.add('selected');
+    }
+
+    highlightThumb(index) {
+        this.deselectThumbs(this.thumbListTarget)
+        this.thumbListTarget.getElementsByTagName('li')[index].classList.add('selected')
+    }
+
+    deselectThumbs(list) {
+        for (const item of list.querySelectorAll('li')) {
+            item.classList.remove('selected');
+        }
+    }
+
 }
